3d16780374ff2706a18b204a9d3f512ca6e0a978
AlissonJaquesLocX <alisson.jaques@locximoveis.com.br>
2022-10-20 16:47:49 -0300

    Adicionando métodos POST e GET básicos, para insersação e consulta de dados na aplicação

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using FilmesAPI.models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+        
+        [HttpPost]
+        public void AdionaFilme([FromBody] Filme filme) {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme);            
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> GetFilmes()
+        {
+            return filmes;
+        }
+    }
+}
